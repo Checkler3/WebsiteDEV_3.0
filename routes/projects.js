28f9f939bc8efaab6409b83bae6b9ccb969cb408
Corey Heckler <50090414+Checkler3@users.noreply.github.com>
2019-09-26 16:53:01 -0400

    Fleshing Out Projects Page

--- routes/projects.js
+++ routes/projects.js
@@ -6,4 +6,8 @@ router.get('/', async (req, res) => {
 	res.render('projects/index', { title: 'Projects' });
 });
 
+router.get('/iss-tracker', async (req, res) => {
+	res.render('projects/iss-tracker', { title: 'Projects - ISS Tracker' });
+});
+
 module.exports = router;
